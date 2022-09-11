6252533c297826d914c04db09ce69c92f167c7a8
Garrett Downs <garredow@outlook.com>
2022-09-11 12:33:04 -0400

    feat: add list header component

---
+++ src/components/ListHeader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<div class="root">
+  {title}
+</div>
+
+<style>
+  .root {
+    text-transform: uppercase;
+    font-weight: 600;
+    padding: 5px;
+  }
+</style>
