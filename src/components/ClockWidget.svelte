9fb9d2472ab592681365322ba121293954448474
Garrett Downs <garredow@outlook.com>
2022-09-14 17:23:59 -0400

    feat(clock): add context menu actions

--- src/components/ClockWidget.svelte
+++ src/components/ClockWidget.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import NavItem from 'onyx-ui/components/nav/NavItem.svelte';
+  import { Onyx } from 'onyx-ui/services';
+
   import { onMount } from 'svelte';
   import type { Widget } from '../models';
   import WidgetPlaceholder from './WidgetPlaceholder.svelte';
@@ -20,15 +23,50 @@
 {#if editing}
   <WidgetPlaceholder id={widget.id} name={widget.name} height={36} {onRemove} {onMove} />
 {:else}
-  <div class="root">
-    <div class="time">{now.toLocaleTimeString()}</div>
-    <div class="date">{now.toLocaleDateString()}</div>
-  </div>
+  <NavItem
+    navi={{
+      itemId: widget.id,
+      onSelect: () => {},
+      onFocus: () => {},
+    }}
+    contextMenu={{
+      title: widget.name,
+      items: [
+        {
+          label: 'Remove',
+          onSelect: () => {
+            onRemove();
+            Onyx.contextMenu.close();
+          },
+        },
+        {
+          label: 'Move Up',
+          onSelect: () => {
+            onMove('up');
+            Onyx.contextMenu.close();
+          },
+        },
+        {
+          label: 'Move Down',
+          onSelect: () => {
+            onMove('down');
+            Onyx.contextMenu.close();
+          },
+        },
+      ],
+    }}
+  >
+    <div class="root">
+      <div class="time">{now.toLocaleTimeString()}</div>
+      <div class="date">{now.toLocaleDateString()}</div>
+    </div>
+  </NavItem>
 {/if}
 
 <style>
   .root {
     text-align: center;
+    padding: 7px;
   }
 
   .time {
